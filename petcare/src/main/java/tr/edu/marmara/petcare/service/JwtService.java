3fd9dd5ceb7d80b54282b63e2b4aa2a54feb0a9d
Emirhan Ese <emirhan_ese@windowslive.com>
2024-04-21 16:16:10 +0300

    fix decode argument error in JwtService

--- petcare/src/main/java/tr/edu/marmara/petcare/service/JwtService.java
+++ petcare/src/main/java/tr/edu/marmara/petcare/service/JwtService.java
@@ -18,7 +18,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
     @Value("${application.security.jwt.secret-key}")
-    private static String SECRET_KEY;
+    private String SECRET_KEY;
     @Value("${application.security.jwt.expiration}")
     private long jwtExpiration;
     @Value("${application.security.jwt.refresh-token.expiration}")
