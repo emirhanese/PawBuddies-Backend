5118c4cc4b088f01ee690d9dce21ca5f59ce602c
Emirhan Ese <emirhan_ese@windowslive.com>
2024-05-05 15:40:33 +0300

    update: dockerfile and docker-compose file updated

--- petcare/src/main/java/tr/edu/marmara/petcare/service/EmailService.java
+++ petcare/src/main/java/tr/edu/marmara/petcare/service/EmailService.java
@@ -40,7 +40,7 @@ public class EmailService {
         if (emailTemplate == null) {
             templateName = "confirm-email";
         } else {
-            templateName = emailTemplate.name();
+            templateName = emailTemplate.getName();
         }
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(
